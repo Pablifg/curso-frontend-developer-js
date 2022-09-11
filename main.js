35a5bfd2af74ca7c52f583302fd74177d3f304aa
Pablo Gallegos <pabli1128@hotmail.com>
2022-09-11 14:52:53 -0500

    Fusión del menú en mobile

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 //elemento al cual daremos clic
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
