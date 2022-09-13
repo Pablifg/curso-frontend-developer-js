3d08383ea1968052f27ed8179943678cf4df44d5
Pablo Gallegos <pabli1128@hotmail.com>
2022-09-13 17:40:14 -0500

    Detalles de un producto

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const menuCarIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -13,14 +13,14 @@ menuCarIcon.addEventListener('click', toggleCartAside);
 /*Garantizo que estén ocultos y muestro el de mi interés*/
 function toggleDesktopMenu() {
     mobileMenu.classList.add('inactive');
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
 
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
     desktopMenu.classList.add('inactive');
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
 
     mobileMenu.classList.toggle('inactive');
 }
@@ -29,7 +29,7 @@ function toggleCartAside() {
     mobileMenu.classList.add('inactive');
     desktopMenu.classList.add('inactive');
 
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 }
 
 //Create array for product list
